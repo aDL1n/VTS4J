d95322d8ffd84877f2d8df18203161e2a7ea7170
aDL1n <111649965+a1n@users.noreply.github.com>
2025-07-26 14:53:57 +0300

    May be it working fine

--- src/main/java/dev/adlin/vts4j/VTSClient.java
+++ src/main/java/dev/adlin/vts4j/VTSClient.java
@@ -8,6 +8,9 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.net.URI;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Queue;
 import java.util.UUID;
 import java.util.concurrent.*;
 import java.util.logging.Logger;
@@ -17,6 +20,9 @@ public class VTSClient {
 
     private final VTSWebSocket webSocket;
     private final ConcurrentHashMap<String, CompletableFuture<JsonObject>> pendingRequests = new ConcurrentHashMap<>();
+    private final HashMap<String, JsonObject> cachedResponses = new HashMap<>();
+    private final Queue<String> idQueue = new ArrayDeque<>();
+    private String currentId = null;
 
     public VTSClient(URI uri) {
         webSocket = new VTSWebSocket(uri);
@@ -54,6 +60,7 @@ public class VTSClient {
         CompletableFuture<JsonObject> future = new CompletableFuture<>();
         String requestId = request.get("requestID").getAsString();
 
+        idQueue.add(requestId);
         pendingRequests.put(requestId, future);
         webSocket.send(request.toString());
 
@@ -61,20 +68,35 @@ public class VTSClient {
     }
 
     private void messageHandle(String message) {
-
         JsonObject json = new Gson().fromJson(message, JsonObject.class);
         String messageType = json.get("messageType").getAsString();
-
         String requestId = json.get("requestID").getAsString();
         CompletableFuture<JsonObject> responseFuture = pendingRequests.remove(requestId);
 
-        if (messageType.equals("APIError")) {
-            JsonObject data = json.getAsJsonObject("data");
-            String errorId = data.get("errorID").getAsString();
-            String errorMessage = data.get("message").getAsString();
-            responseFuture.completeExceptionally(new VTSException(errorId + " " + errorMessage));
+        if (currentId == null) currentId = idQueue.poll();
+
+        if (!requestId.equals(currentId)) {
+            cachedResponses.put(requestId, json);
         } else {
-            responseFuture.complete(json);
+            if (messageType.equals("APIError")) {
+                JsonObject data = json.getAsJsonObject("data");
+                String errorId = data.get("errorID").getAsString();
+                String errorMessage = data.get("message").getAsString();
+                responseFuture.completeExceptionally(new VTSException(errorId + " " + errorMessage));
+            } else {
+                responseFuture.complete(json);
+            }
+
+            checkCache();
         }
+
+        System.out.println(currentId);
+        currentId = null;
+    }
+
+    private void checkCache() {
+        if (cachedResponses.isEmpty()) return;
+
+        messageHandle(cachedResponses.get(idQueue.peek()).getAsString());
     }
 }
